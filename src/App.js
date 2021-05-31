55e48adbdd0586637c939ced41bd078291eaf7a9
Roopali Singh <roopali.singh.222@gmail.com>
2021-05-31 23:29:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+// STARTED ON 26th January 2021
+import React from "react";
+import "./App.css";
+import Navbar from "./Navbar";
+import Banner from "./Banner";
+import Row from "./Row";
+import requests from "./request";
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
+
+function Navbar() {
+
+    const [show, setShow] = useState();
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setShow(true);
+      } else setShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://www.freepnglogos.com/uploads/netflix-tv-logo-png-9.png"
+        alt="Netflix Logo"
+      />
+      <img
+        className="nav__avatar"
+        src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/366be133850498.56ba69ac36858.png"
+        alt="Netflix Account"
+      />
+    </div>
+  );
+}
+
+export default Navbar;
